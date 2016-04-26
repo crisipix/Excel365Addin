81db5425c0b8ce3dfc738ffe73ea5fcba43d54e0
crisipix <wongc5@gmail.com>
2016-04-26 00:08:02 -0400

    prevent ng-repeat from throwing dups error.

--- Excel365AddinWeb/App/Management/ManageController.js
+++ Excel365AddinWeb/App/Management/ManageController.js
@@ -8,20 +8,7 @@
     
     var managerService = function ($q) {
         //Reads data from current document selection and displays a notification
-        this.getDataFromSelection = function () {
-            var deferred = $q.defer();
-            Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-               function (result) {
-                   if (result.status === Office.AsyncResultStatus.Succeeded) {
-                       deferred.resolve(result.value.split('\n'));
-                   }
-                   else {
-                       deferred.reject([]);
-                   }
-               });
-            return deferred.promise;
-        }
-
+      
         this.sendDataFromSelection = function () {
             var deferred = $q.defer();
             Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
@@ -29,13 +16,10 @@
                    if (result.status === Office.AsyncResultStatus.Succeeded) {
                        deferred.resolve(result.value.split('\n'));
                    }
-                    else {
-                       deferred.reject([]);
-                   }
+                    else { deferred.reject([]); }
                });
             return deferred.promise;
         }
-
     }
 
     var manageCtrl = function ($scope, managerService) {
@@ -45,28 +29,11 @@
         vm.message = {header:'', body:''};
         vm.test = function() { vm.showMessage = true; vm.message.body ="HELLOOOOOO" };
         vm.results = [];
-        $scope.$watchCollection('results', function (newValue, oldValue) { vm.results = newValue; });
+        //$scope.$watchCollection('vm.results', function (newValue, oldValue) {
+        //    vm.results = angular.copy(newValue);
+        //});
 
-        function getDataFromSelection() {
-            managerService.getDataFromSelection().then(
-                function (result) {
-                    vm.showMessage = true;
-
-                    if (result.length === 0) {
-                        vm.message.body = 'There was no selected text';
-                    }
-                    else {
-                        vm.message = { header: 'The selected text was sent to the server:', body: result.join(' ')  };
-                        vm.results = result;
-                    }
-                },
-                function (error) {
-                    vm.message = { header: 'Error:', body: error.message };
-                }
-                );
-           
-
-        }
+        
 
         function sendDataFromSelection() {
             managerService.sendDataFromSelection().then(
@@ -78,9 +45,12 @@
                    }
                    else {
                        vm.message = { header: 'The selected text was sent to the server:', body: result.join(' ') };
-                       vm.results = result;
+                      vm.results = result;
+                      //vm.results = angular.copy(result);
 
-                      // $scope.$apply();
+                       //var element = angular.element($('#resultsRow'));
+                       //element.scope().$apply();
+                       //$scope.$apply();
                    }
                },
                function (error) {
@@ -89,10 +59,7 @@
                );
         }
 
-        vm.getDataFromSelection = getDataFromSelection;
         vm.sendDataFromSelection = sendDataFromSelection;
-        
-
     }
 
     angular.module('appMain').service('managerService', managerService);
