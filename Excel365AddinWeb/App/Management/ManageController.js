d4d977ae32a0c8c84f99c506f7895279f7f47f54
crisipix <wongc5@gmail.com>
2016-04-26 00:27:41 -0400

    added cell updates.

--- Excel365AddinWeb/App/Management/ManageController.js
+++ Excel365AddinWeb/App/Management/ManageController.js
@@ -20,6 +20,48 @@
                });
             return deferred.promise;
         }
+
+        this.updateRange = function ()
+        {
+            var deferred = $q.defer();
+
+            // Run a batch operation against the Excel object model. Use the context argument to get access to the Excel document.
+            Excel.run(function (ctx) {
+
+                // Create a proxy object for the sheet
+                var sheet = ctx.workbook.worksheets.getActiveWorksheet();
+                // Values to be updated
+                var values = [
+                             ["Type", "Estimate"],
+                             ["Transportation", 1670],
+                             ["Food", 800],
+                             ["Fuel", 1111]
+                ];
+                // Create a proxy object for the range
+                var range = sheet.getRange("A1:B4");
+
+                // Assign array value to the proxy object's values property.
+                range.values = values;
+
+                // Queue a command to load the text property for the proxy range object.    
+                range.load('text');
+
+                // Synchronizes the state between JavaScript proxy objects and real objects in Excel by executing instructions queued on the context 
+                return ctx.sync().then(function () {
+                    console.log("Done");
+                    deferred.resolve();
+
+                });
+            }).catch(function (error) {
+                console.log("Error: " + error);
+                if (error instanceof OfficeExtension.Error) {
+                    console.log("Debug info: " + JSON.stringify(error.debugInfo));
+                }
+            });
+
+            return deferred.promise;
+
+        }
     }
 
     var manageCtrl = function ($scope, managerService) {
@@ -32,8 +74,6 @@
         //$scope.$watchCollection('vm.results', function (newValue, oldValue) {
         //    vm.results = angular.copy(newValue);
         //});
-
-        
 
         function sendDataFromSelection() {
             managerService.sendDataFromSelection().then(
@@ -57,6 +97,12 @@
                    vm.message = { header: 'Error:', body: error.message };
                }
                );
+        }
+
+
+        vm.updateRange = function ()
+        {
+            managerService.updateRange();
         }
 
         vm.sendDataFromSelection = sendDataFromSelection;
